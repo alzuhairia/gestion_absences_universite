b5e5fea4385d67cf384fb67875a8da811109e306
Alzuhairi <ahmed.alzuhairi@hainaut-promsoc.be>
2026-02-21 20:30:46 +0100

    test: make deploy check resilient to local dev overrides

--- tests/test_ci_checks.py
+++ tests/test_ci_checks.py
@@ -2,6 +2,7 @@ from io import StringIO
 
 from django.core.management import call_command
 from django.test import SimpleTestCase
+from django.test.utils import override_settings
 
 
 class MigrationCheckTests(SimpleTestCase):
@@ -26,12 +27,21 @@ class MigrationCheckTests(SimpleTestCase):
         stdout = StringIO()
         stderr = StringIO()
 
-        call_command(
-            'check',
-            deploy=True,
-            stdout=stdout,
-            stderr=stderr,
-        )
+        with override_settings(
+            DEBUG=False,
+            SECURE_SSL_REDIRECT=True,
+            SECURE_HSTS_SECONDS=31536000,
+            SECURE_HSTS_INCLUDE_SUBDOMAINS=True,
+            SECURE_HSTS_PRELOAD=True,
+            SESSION_COOKIE_SECURE=True,
+            CSRF_COOKIE_SECURE=True,
+        ):
+            call_command(
+                'check',
+                deploy=True,
+                stdout=stdout,
+                stderr=stderr,
+            )
 
         combined_output = f"{stdout.getvalue()}\n{stderr.getvalue()}".lower()
         self.assertNotIn('warning', combined_output)
